2e674766dbb2e38032408d1f0fae066e10a904ce
Gagaro <gagaro42@gmail.com>
2014-10-29 10:58:48 +0100

    migration

---
+++ admin_watchdog/migrations/0002_auto_20141029_1058.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('admin_watchdog', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='logentry',
+            options={'verbose_name': 'Log entry', 'verbose_name_plural': 'Log entries'},
+        ),
+    ]
